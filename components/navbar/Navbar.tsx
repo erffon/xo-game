0339f9de7c0592370b9c380a728d0f7bd75b6fc5
office <erffon@gmail.com>
2022-11-13 00:20:36 +0330

    navbar created

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,15 +3,19 @@ import { BsMoon } from "react-icons/bs";
 function Navbar() {
   return (
     <nav className="container flex pt-8 items-center">
-      <div className="flex-1">
+      <div className="flex flex-1 items-center gap-7">
         <img src="/logo96.png" alt="XO game logo" className="w-16" />
+        <p className="text-black font-bold leading-4 text-lg">
+          Tic Tac Toe <br />
+          (XO) game
+        </p>
       </div>
       <div className="flex gap-8">
-        <button className="flex-1 border border-primary-light px-5 py-1 rounded-lg">
+        <button className="flex-1 border border-primary-light px-5 py-1 rounded-lg text-texts-light hover:shadow-md">
           About
         </button>
-        <div className="w-10 h-10 rounded-full border border-borders">
-          <BsMoon className="relative top-0 left-0" />
+        <div className="flex w-10 h-10 rounded-full border border-borders justify-center items-center bg-secondary-light cursor-pointer hover:shadow-md">
+          <BsMoon className="text-xl text-texts-light" />
         </div>
       </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,31 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "10rem",
+      padding: "15rem",
     },
-    extend: {},
-    colors: {
-      primary: {
-        light: "#00976A",
-        dark: "#4EBE86",
-      },
-      secondary: {
-        light: "rgb(0,0,0,5%)",
-        dark: "rgb(255,255,255,10%)",
-      },
-      redish: "#F44336",
-      borders: "rgb (161,161,161,50%)",
-      "wrapper-light": "rgb (0,0,0,5%)",
-      "wrapper-dark": "rgb (255,255,255,10%)",
-      background: {
-        light: "radial-gradient(50% 50% at 50% 50%, #8AE8CC 0%, #EEEEEE 100%)",
-        dark: "radial-gradient(50% 50% at 50% 50%, #021B0F 0%, #000517 100%)",
+    extend: {
+      colors: {
+        primary: {
+          light: "#00976A",
+          dark: "#4EBE86",
+        },
+        secondary: {
+          light: "rgb(0,0,0,5%)",
+          dark: "rgb(255,255,255,10%)",
+        },
+        redish: "#F44336",
+        borders: "rgb(161,161,161,50%)",
+        "wrapper-light": "rgb (0,0,0,5%)",
+        "wrapper-dark": "rgb (255,255,255,10%)",
+        background: {
+          light:
+            "radial-gradient(50% 50% at 50% 50%, #8AE8CC 0%, #EEEEEE 100%)",
+          dark: "radial-gradient(50% 50% at 50% 50%, #021B0F 0%, #000517 100%)",
+        },
+        texts: {
+          light: "rgb(37, 37, 37, 80%)",
+          dark: "rgb(255, 255, 255, 80%)",
+        },
       },
     },
   },
