3cde88112d66beb431e87ab0516b7713e02e1d53
office <erffon@gmail.com>
2022-11-12 23:17:31 +0330

    design system colors added

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 import logo from "../../public/logo48.png";
 function Navbar() {
   return (
-    <nav className="container">
-      <Image src={logo} alt="XO game logo" placeholder="blur" />
+    <nav className="container flex">
+      <Image
+        src={logo}
+        alt="XO game logo"
+        placeholder="blur"
+        className="pt-8"
+      />
+      <div>
+        <button className="border">About</button>
+      </div>
     </nav>
   );
 }
