8bf732e4722997649431151815dcefa53172a62b
office <erffon@gmail.com>
2022-11-12 23:48:01 +0330

    nav changes

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,18 @@
-import Image from "next/image";
-import logo from "../../public/logo48.png";
+import { BsMoon } from "react-icons/bs";
+
 function Navbar() {
   return (
-    <nav className="container flex">
-      <Image
-        src={logo}
-        alt="XO game logo"
-        placeholder="blur"
-        className="pt-8"
-      />
-      <div>
-        <button className="border">About</button>
+    <nav className="container flex pt-8 items-center">
+      <div className="flex-1">
+        <img src="/logo96.png" alt="XO game logo" className="w-16" />
+      </div>
+      <div className="flex gap-8">
+        <button className="flex-1 border border-primary-light px-5 py-1 rounded-lg">
+          About
+        </button>
+        <div className="w-10 h-10 rounded-full border border-borders">
+          <BsMoon className="relative top-0 left-0" />
+        </div>
       </div>
     </nav>
   );
