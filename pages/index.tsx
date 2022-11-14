52003f6cc273df40aab668129d60498487828665
office <erffon@gmail.com>
2022-11-14 10:01:07 +0330

    some adjustments added

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,28 @@ export default function Home() {
         <link rel="icon" href="/xo.ico" />
       </Head>
       <Navbar />
-      <div className="flex justify-center items-center h-5/6">
-        <div className="flex gap-3 border border-borders rounded-2xl w-2/5 py-12 px-10 justify-center items-center backdrop-blur-sm ">
-          <img
-            src="./images/waving-hand-emoji.png"
-            alt="greetings to the users"
-            className="w-10"
-          />
-          <h2 className="text-4xl">Hi</h2>
+      <main className="flex h-4/6 justify-center items-center">
+        <div className="flex flex-col justify-center items-center border border-borders rounded-2xl py-12 px-10 w-2/5 backdrop-blur-sm">
+          <div className="flex gap-3  ">
+            <img
+              src="./images/waving-hand-emoji.png"
+              alt="greetings to the users"
+              className="w-10"
+            />
+            <h2 className="text-4xl">Hi</h2>
+          </div>
+          <div>
+            <p>welcome to XO game</p>
+            <p>please enter your name:</p>
+          </div>
+          <div>
+            <input type="button" value="enter your name here" />
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
