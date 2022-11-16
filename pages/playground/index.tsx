eebc8903e6eb715cec26dce80c5e4fed469ed302
office <erffon@gmail.com>
2022-11-17 00:33:11 +0330

    some small changes

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -5,19 +5,21 @@ import { BiCircle, BiX } from "react-icons/bi";
 const Playground = () => {
   const tiles = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   return (
-    <div className="bg-light">
+    <div className="bg-light h-screen overflow-hidden">
       <Navbar />
       <div className="container">
-        <div className="flex flex-row flex-wrap mt-20 justify-center align-middle gap-5 px-16">
+        <div className="flex flex-row flex-wrap mt-10 justify-center items-center gap-5 px-80">
           {tiles.map((item) => {
             return (
-              <Container className="w-24 h-24 text-center">{item}</Container>
+              <Container className="flex justify-center items-center w-24 h-24">
+                {item}
+              </Container>
             );
           })}
         </div>
 
         {/* ---------------------------- YOU and CPU icons --------------------------- */}
-        <div className="flex flex-row justify-center gap-72 mt-16">
+        <div className="flex flex-row justify-center gap-40 mt-10">
           <div className="flex flex-col items-center justify-center">
             <BiCircle className="text-7xl text-gray-400 mb-5" />
             <h3 className="text-2xl text-gray-500">YOU</h3>
