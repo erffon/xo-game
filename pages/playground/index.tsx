25703d26425099ee1a4e7f4169cac1765b7fb394
office <erffon@gmail.com>
2022-11-16 23:54:13 +0330

    some small changes

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -5,28 +5,30 @@ import { BiCircle, BiX } from "react-icons/bi";
 const Playground = () => {
   const tiles = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   return (
-    <>
+    <div className="bg-light">
       <Navbar />
       <div className="container">
-        <div className="flex flex-row mx-52 flex-wrap">
+        <div className="flex flex-row flex-wrap mt-20 justify-center align-middle gap-5 px-16">
           {tiles.map((item) => {
-            return <Container className="flex-1">item</Container>;
+            return (
+              <Container className="w-24 h-24 text-center">{item}</Container>
+            );
           })}
         </div>
 
         {/* ---------------------------- YOU and CPU icons --------------------------- */}
-        <div className="flex flex-row justify-center gap-72 mt-20">
+        <div className="flex flex-row justify-center gap-72 mt-16">
           <div className="flex flex-col items-center justify-center">
-            <BiCircle className="text-8xl text-gray-400 mb-5" />
-            <h3 className="text-4xl text-gray-500">YOU</h3>
+            <BiCircle className="text-7xl text-gray-400 mb-5" />
+            <h3 className="text-2xl text-gray-500">YOU</h3>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <BiX className="text-9xl text-gray-400 " />
-            <h3 className="text-4xl text-gray-500">CPU</h3>
+            <BiX className="text-8xl text-gray-400 " />
+            <h3 className="text-2xl text-gray-500">CPU</h3>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
