24711b6964604d82fd8ac2fff63fb38854d9549c
office <erffon@gmail.com>
2022-11-18 18:17:09 +0330

    some changes made

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -26,50 +26,16 @@ const Playground = () => {
   };
 
   return (
-    <div className="bg-light h-screen overflow-hidden">
-      <Navbar />
-      <div className="container">
-        <div className="flex flex-row flex-wrap mt-10 justify-center items-center gap-5 px-80">
-          {resultTiles.map((item) => {
-            let result;
-            switch (item) {
-              case 1:
-                result = <BiCircle className="text-6xl text-redish" />;
-                break;
-              case 2:
-                result = <BiX className="text-8xl text-primary-light" />;
-                break;
-              default:
-                result = "";
-                break;
-            }
-            return (
-              <Container
-                className={`flex justify-center items-center w-24 h-24 ${
-                  item == 0
-                    ? "hover:border-2 hover:shadow-sm active:border-redish active:border-4"
-                    : ""
-                }`}
-              >
-                {result}
-              </Container>
-            );
-          })}
+    <>
+      <h1>React Tic Tac Toe - With Hooks</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>History</h3>
         </div>
-
-        {/* ---------------------------- YOU and CPU icons --------------------------- */}
-        <div className="flex flex-row justify-center gap-40 mt-10">
-          <div className="flex flex-col items-center justify-center">
-            <BiCircle className="text-7xl text-gray-400 mb-5" />
-            <h3 className="text-2xl text-gray-500">YOU</h3>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <BiX className="text-8xl text-gray-400 " />
-            <h3 className="text-2xl text-gray-500">CPU</h3>
-          </div>
-        </div>
+        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
       </div>
-    </div>
+    </>
   );
 };
 
