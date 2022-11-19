4618e5f19f3336dde0f6d93dbf7ba0a19f480a59
office <erffon@gmail.com>
2022-11-19 13:36:04 +0330

    some changes in square file

--- pages/playground/Square.tsx
+++ pages/playground/Square.tsx
@@ -8,11 +8,7 @@ import React from "react";
 const Square = ({ value, onClick }) => {
   const style = value ? `squares ${value}` : `squares`;
 
-  return (
-    <button className={style} onClick={onClick}>
-      {value}
-    </button>
-  );
+  return <button onClick={onClick}>{value}</button>;
 };
 
 export default Square;
