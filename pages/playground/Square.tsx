5b2fb9bc10c836b8127e2e293c70c1feb102bb0b
office <erffon@gmail.com>
2022-11-19 14:18:04 +0330

    square component done

--- pages/playground/Square.tsx
+++ pages/playground/Square.tsx
@@ -6,8 +6,6 @@ import React from "react";
 // }
 
 const Square = ({ value, onClick }) => {
-  const style = value ? `squares ${value}` : `squares`;
-
   return <button onClick={onClick}>{value}</button>;
 };
 
