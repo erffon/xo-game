88f221679295cdf9521342ec326a1c42aaacd08a
office <erffon@gmail.com>
2022-11-19 22:23:58 +0330

    some states added to game

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Board from "./Board";
 import Square from "./Square";
+import { calculateWinner } from "./CalculateWinner";
 
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
   const handleClick = () => {};
   const jumpTo = () => {};
   const renderMoves = () => {};
