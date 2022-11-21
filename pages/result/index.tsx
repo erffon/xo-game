fb9b9870c680418ec96da72a39463c6401740c88
office <erffon@gmail.com>
2022-11-21 14:04:37 +0330

    result page design added

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,9 +1,31 @@
 import { useRouter } from "next/router";
+import Navbar from "../../components/navbar/Navbar";
+import Container from "../../components/Ui/Container";
 
 const Result = () => {
-  const router = useRouter();
+  const { query, push } = useRouter();
 
-  return <div>{router.query.winner}</div>;
+  return (
+    <div className="bg-light h-screen overflow-hidden font-inter">
+      <Navbar isHome={true} />
+      <main className="flex h-4/6 justify-center items-center">
+        <Container className="flex flex-col gap-5 py-12 px-10 w-2/5 justify-center items-center">
+          <div className="flex gap-3">
+            <img
+              src="./images/waving-hand-emoji.png"
+              alt="greetings to the users"
+              className="w-10 hover:-rotate-6 duration-300 hover:scale-110 "
+            />
+            <h2 className="text-4xl font-bold text-texts-light">Hi</h2>
+          </div>
+          <div className="text-texts-light text-center text-xl ">
+            <p className="tracking-widest font-light">welcome to XO game</p>
+            <p className="font-medium">please enter your name:</p>
+          </div>
+        </Container>
+      </main>
+    </div>
+  );
 };
 
 export default Result;
