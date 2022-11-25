e6e86448027be46315990609886ed02e99e11e2c
office <erffon@gmail.com>
2022-11-25 17:18:36 +0330

    players names fixed

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Board from "./Board";
 import calculateWinner from "./CalculateWinner";
@@ -8,6 +8,14 @@ const Game = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const winner = calculateWinner(board);
   const { push } = useRouter();
+
+  const [firstPlayer, setFirstPlayer] = useState<string | null>("");
+  const [secondPlayer, setSecondPlayer] = useState<string | null>("");
+
+  useEffect(() => {
+    setFirstPlayer(window.localStorage.getItem("player1"));
+    setSecondPlayer(window.localStorage.getItem("player2"));
+  }, []);
 
   const handleClick = (i: number) => {
     const boardCopy: string[] = [...board];
@@ -34,13 +42,11 @@ const Game = () => {
         <div className="flex justify-center gap-24 text-slate-400 text-center mt-16">
           <div className="flex flex-col gap-1">
             <p className="text-5xl font-inter font-medium">O</p>
-            <p className="text-2xl font-semibold">
-              {window.localStorage.getItem("name")}
-            </p>
+            <p className="text-2xl font-semibold">{firstPlayer}</p>
           </div>
           <div className="flex flex-col gap-1">
             <p className="text-5xl font-inter font-medium">X</p>
-            <p className="text-2xl font-semibold">Player2</p>
+            <p className="text-2xl font-semibold">{secondPlayer}</p>
           </div>
         </div>
       </div>
